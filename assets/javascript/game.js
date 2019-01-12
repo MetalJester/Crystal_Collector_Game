38dc6212d630a3ed81001cb624fe655c40fb9686
MetalJester <acjswanson@gmail.com>
2019-01-12 15:00:13 -0800

    added variables for each crystal and the computer to generate random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,27 @@
 console.log("I am here");
 
 
-$(document).ready(function(){
-    $(".crystal").on("click", function(){
+var crystalNum1 = Math.floor((Math.random() * 12) + 1);
+var crystalNum2 = Math.floor((Math.random() * 12) + 1);
+var crystalNum3 = Math.floor((Math.random() * 12) + 1);
+var crystalNum4 = Math.floor((Math.random() * 12) + 1);
+var randomNumber = Math.floor((Math.random() * 102) + 19);
+   
+
+console.log(crystalNum1);
+console.log(crystalNum2);
+console.log(crystalNum3);
+console.log(crystalNum4);
+console.log(randomNumber);
+
+
+
+$(document).ready(function () {
+    $(".crystal").on("click", function () {
         alert("I've been clicked");
-      
+
     });
-  });
+
+
+});
 
